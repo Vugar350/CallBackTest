077796ccc3e110e99d5ff3551020102796085176
vugar350 <Vugar350@mail.ru>
2022-02-21 21:04:24 +0300

    initial commit

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,2 +1,46 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+
+    }
+
+    @BeforeEach
+     public void setUp() {
+        driver =new ChromeDriver();
+    }
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver=null;
+    }
+    @Test
+    public void shouldTestSomething() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Василий");
+        elements.get(1).sendKeys("+79012345678");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+    }
+
+
 }
